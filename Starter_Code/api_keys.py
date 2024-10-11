b70d2c3e3a1a6b5595a73ec00d01ebb92798b749
Ellen Grant <ellenckgrant@gmail.com>
2024-10-10 20:33:35 -0500

    push update

--- Starter_Code/api_keys.py
+++ Starter_Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "f3c2c416f44826e26471ffe5510a1e0e"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "228eab7a3a4c48598657a25573c15a17"
